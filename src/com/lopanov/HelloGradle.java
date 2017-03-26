59ee2bdc4fb43c3788782b185ec40036b0726e21
lopanov <lopanov@kamaz.org>
2017-03-26 14:42:06 +0300

    создание проекта HelloGradle в IntelliJ IDEA

---
+++ src/com/lopanov/HelloGradle.java
@@ -0,0 +1,11 @@
+package com.lopanov;
+
+public class HelloGradle {
+    public HelloGradle() {
+        System.out.println("Hello Gradle");
+    }
+
+    public static void main() {
+        new HelloGradle();
+    }
+}
